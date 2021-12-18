f8b10d21fac1128b6ac45aba51f58b3991c75baa
Gerombo666 <44369699+Gerombo666@users.noreply.github.com>
2021-12-18 19:26:36 +0300

    Add files via upload

--- server/config/nodemailer.config.js
+++ server/config/nodemailer.config.js
@@ -2,8 +2,8 @@ const nodemailer = require('nodemailer');
 
 module.exports = {
     transporter: nodemailer.createTransport({
-        service: 'gmail',
-        host: 'smtp.gmail.com',
+        service: 'yandex',
+        host: 'smtp.yandex.ru',
         auth: {
             user: process.env.EMAIL_LOGIN, // email login
             pass: process.env.EMAIL_PASSWORD // email password
